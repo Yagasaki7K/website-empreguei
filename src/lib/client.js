3c8825e01b4261fdeeb92672ee8d103fdf4f2635
Anderson Marlon <anderson18.marlon@gmail.com>
2023-06-13 13:21:40 -0300

    [Firefox] Could not reach Cloud Firestore backend. Connection failed 1 times

--- src/lib/client.js
+++ src/lib/client.js
@@ -1,18 +1,19 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { initializeFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.PROJECT_ID,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
-const database = getFirestore(app, {
-  experimentalForceLongPolling: true,
+const database = initializeFirestore(app, {
+  experimentalForceLongPolling: true, // this line
+  useFetchStreams: false, // and this line
 });
 
 export default database;
